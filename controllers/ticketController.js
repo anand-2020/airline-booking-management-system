5bd8b436fdaab02a3e96fb530af2cadad8b35a60
anand-2020 <62353883+anand-2020@users.noreply.github.com>
2022-04-12 17:48:44 +0530

    implemented multiple tickets booking

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -3,27 +3,33 @@ import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
 
 const bookTicket = catchAsync(async (req, res, next) => {
-  const query = `INSERT INTO TICKET 
+  //TODO: write trigger for stopping insertion if flight_status is 'CANCELLED'
+
+  let query = `INSERT INTO TICKET 
   (FLIGHT_ID, CUSTOMER_ID, PASSENGER_NAME, TIME_OF_BOOKING, DATE_OF_DEPARTURE, FARE, SEAT_NUMBER, STATUS) 
-  VALUES(?, ?, ?, ?, ?, ?, ?, ?)`;
+  VALUES `;
 
-  const params = [
-    req.body.flight_id,
-    req.body.customer_id, // change this to req.customer.customer_id when protect function in authController is written
-    req.body.passenger_name,
-    new Date(),
-    req.body.date_of_departure,
-    req.body.fare,
-    req.body.seat_number,
-    "BOOKED",
-  ];
+  const dt = new Date().toISOString().slice(0, 19).replace("T", " ");
+  req.body.tickets.forEach((ticket) => {
+    query += ` ( 
+      '${req.body.flight_id}', 
+      '${req.body.customer_id}', 
+      '${ticket.passenger_name}', 
+      '${dt}', 
+      '${req.body.date_of_departure}', 
+      ${ticket.fare}, 
+      ${ticket.seat_number}, 
+      'BOOKED' ),`;
+  });
 
-  const bookedTicket = await db.executeQuery(query, params);
+  const bookedTicket = await db.executeQuery(
+    query.substring(0, query.length - 1)
+  );
 
   res.status(200).json({
     status: "success",
     data: {
-      ticket_id: bookedTicket.data.insertId,
+      ticketId_start: bookedTicket.data.insertId,
     },
   });
 });
@@ -54,7 +60,9 @@ const getTicket = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: "success",
-    ticket,
+    data: {
+      ticket: ticket.data[0],
+    },
   });
 });
 
@@ -78,23 +86,24 @@ const cancelTicket = catchAsync(async (req, res, next) => {
 const bookedTicketsForFlight = catchAsync(async (req, res, next) => {
   const params = [req.params.flightId, req.params.dateOfDeparture];
 
-  const flightQuery = `SELECT FLIGHT_PATH.*, FLIGHT_DATE.DEPARTURE_DATE,  
-  SRC.AIRPORT_ID AS SRC_AIRPORT_ID, 
+  const dt = new Date(req.params.dateOfDeparture);
+  const day = (dt.getDay() + 1).toString();
+
+  const flightQuery = `SELECT FLIGHT_PATH.*,  
   SRC.AIRPORT_NAME AS SRC_AIRPORT_NAME, 
   SRC.CITY AS SRC_CITY, 
   SRC.COUNTRY AS SRC_COUNTRY, 
-  DEST.AIRPORT_ID AS DEST_AIRPORT_ID, 
   DEST.AIRPORT_NAME AS DEST_AIRPORT_NAME, 
   DEST.CITY AS DEST_CITY, 
   DEST.COUNTRY AS DEST_COUNTRY 
-  FROM FLIGHT_DATE, FLIGHT_PATH, AIRPORT SRC, AIRPORT DEST 
-  WHERE FLIGHT_DATE.FLIGHT_ID = ? AND 
-  STR_TO_DATE(FLIGHT_DATE.DEPARTURE_DATE, '%Y-%m-%d') = STR_TO_DATE(? , '%Y-%m-%d')  
-  AND FLIGHT_DATE.FLIGHT_ID = FLIGHT_PATH.FLIGHT_ID 
+  FROM FLIGHT_DAY, FLIGHT_PATH, AIRPORT SRC, AIRPORT DEST 
+  WHERE FLIGHT_DAY.FLIGHT_ID = ? 
+  AND FLIGHT_DAY.DAY_OF_WEEK = ?  
+  AND FLIGHT_DAY.FLIGHT_ID = FLIGHT_PATH.FLIGHT_ID 
   AND FLIGHT_PATH.SOURCE_ID = SRC.AIRPORT_ID 
   AND FLIGHT_PATH.DESTINATION_ID = DEST.AIRPORT_ID`;
 
-  const flight = await db.executeQuery(flightQuery, params);
+  const flight = await db.executeQuery(flightQuery, [req.params.flightId, day]);
 
   if (flight.data.length === 0) {
     return next(
@@ -105,15 +114,19 @@ const bookedTicketsForFlight = catchAsync(async (req, res, next) => {
     );
   }
 
+  flight.data[0].DEPARTURE_DATE = req.params.dateOfDeparture;
+
   const query = `SELECT TICKET_ID, PASSENGER_NAME, TIME_OF_BOOKING, FARE, SEAT_NUMBER, STATUS 
   FROM TICKET WHERE FLIGHT_ID = ? AND DATE_OF_DEPARTURE = ?`;
 
-  const bookedTickets = await db.executeQuery(query, params);
+  const tickets = await db.executeQuery(query, params);
 
   res.status(200).json({
     status: "success",
-    flight,
-    bookedTickets,
+    data: {
+      flight: flight.data[0],
+      tickets: tickets.data,
+    },
   });
 });
 
