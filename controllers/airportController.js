596cae2c429d71e4c632a630f8abb969ad475214
anand-2020 <62353883+anand-2020@users.noreply.github.com>
2022-04-16 14:52:19 +0530

    added airport routes

---
+++ controllers/airportController.js
@@ -0,0 +1,30 @@
+import db from "../models/database.js";
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+
+export const getAllAirports = catchAsync(async (req, res, next) => {
+  const query = `SELECT * FROM AIRPORT `;
+  const airports = await db.executeQuery(query);
+
+  res.status(200).json({
+    status: "success",
+    data: airports.data,
+  });
+});
+
+export const addAirport = catchAsync(async (req, res, next) => {
+  const query = `INSERT INTO AIRPORT VALUES (?, ?, ?, ?, ?)`;
+  const params = [
+    req.body.AIRPORT_ID,
+    req.body.AIRPORT_NAME,
+    req.body.CITY,
+    req.body.COUNTRY,
+    req.body.OFFSET,
+  ];
+
+  await db.executeQuery(query, params);
+
+  res.status(200).json({
+    status: "success",
+  });
+});
